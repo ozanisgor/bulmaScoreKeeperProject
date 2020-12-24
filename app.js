4777571d2495e584db0e3cd052ec26935cadcaab
ozan isgor <ozanisgor11@gmail.com>
2020-12-24 12:09:36 +0300

    winning score change and reset game functionalities added

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset');
+const selectToPlay = document.querySelector('#selectToPlay');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function() {
@@ -28,4 +30,18 @@ p2Button.addEventListener('click', function() {
   }
 })
 
+selectToPlay.addEventListener('change', function() {
+  winningScore = parseInt(this.value);
+  reset();
+})
+
+resetButton.addEventListener('click', reset);
+
+function reset() {
+  isGameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+}
 
